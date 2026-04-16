a883cde15ea81f9f3694db48703d91fe31a2e48a
alfonsoesrepro <alfonsoesrepro@gmail.com>
2026-04-16 12:30:20 -0300

    Clase Cola y Nodo.

---
+++ pryEDSilvaA/clsCola.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryEDSilvaA
+{
+    internal class clsCola
+    {
+        //Campos
+        private clsNodo pri;
+        private clsNodo ult;
+
+        //Propiedades
+        public clsNodo Primero { get => pri; set => pri = value; }
+        public clsNodo Ultimo { get => ult; set => ult = value; }
+
+        /*
+        public clsNodo Primero
+        {
+            get {return pri;}
+            set {pri = value;}
+        }
+
+        public clsNodo Ultimo
+        {
+            get {return ult;}
+            set {ult = value;}
+        }
+        */
+
+        public clsNodo Agregar(clsNodo nuevoElemento)
+        {
+            if (Primero == null)
+            {
+                Primero = nuevoElemento;
+                Ultimo = nuevoElemento;
+            }
+            else
+            {
+                Ultimo.Siguiente = nuevoElemento;
+                Ultimo = nuevoElemento;
+            }
+            return nuevoElemento;
+        }
+
+        public void Eliminar()
+        {
+            if (Primero == Ultimo)
+            {
+                Primero = null;
+                Ultimo = null;
+            }
+            else
+            {
+                Primero = Primero.Siguiente;
+            }
+        }
+
+
+        /*
+        public clsNodo Eliminar()
+        {
+            if (Primero != Ultimo)
+            {
+                clsNodo nodoEliminado = Primero;
+                Primero = Primero.Siguiente;
+                nodoEliminado.Siguiente = null; // Desconectar el nodo eliminado
+                return nodoEliminado;
+            }
+            else
+            {
+                return null;
+            }
+        }
+          
+        */
+
+
+        public void Recorrer(DataGridView dgv)
+        {
+            clsNodo Aux = Primero;
+            while (Aux != null)
+            {
+                dgv.Rows.Add(Aux.Codigo, Aux.Nombre, Aux.Tramite);
+                Aux = Aux.Siguiente;
+            }
+        }
+
+        public void Recorrer(ListBox lb)
+        {
+            clsNodo Aux = Primero;
+            while (Aux != null)
+            {
+                lb.Items.Add($"Código: {Aux.Codigo}, Nombre: {Aux.Nombre}, Trámite: {Aux.Tramite}");
+                Aux = Aux.Siguiente;
+            }
+        }
+
+        public void Recorrer(ComboBox cb
+            )
+        {
+            clsNodo Aux = Primero;
+            while (Aux != null)
+            {
+                cb.Items.Add($"Código: {Aux.Codigo}, Nombre: {Aux.Nombre}, Trámite: {Aux.Tramite}");
+                Aux = Aux.Siguiente;
+            }
+        }
+
+        /*public void Recorrer()
+        
+            clsNodo Aux = Primero;
+            while (Aux != null)
+            {
+                dgv.Rows.Add(Aux.Codigo, Aux.Nombre, Aux.Tramite);
+                Aux = Aux.Siguiente;
+            }
+        }*/
+    }
+}
---
+++ pryEDSilvaA/clsNodo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryEDSilvaA
+{
+    internal class clsNodo
+    {
+        //Campos
+        private Int32 cod;
+        private String nom;
+        private String tra;
+        private clsNodo sig;
+
+        //Propiedades
+        public Int32 Codigo { get => cod; set => cod = value; }
+        public String Nombre { get => nom; set => nom = value; }
+        public String Tramite { get => tra; set => tra = value; }
+        public clsNodo Siguiente { get => sig; set => sig = value; }
+
+        /*
+        public Int32 Codigo
+        {
+            get {return cod;}
+            set {cod = value;}
+        }
+        public String Nombre
+        {
+            get {return nom;}
+            set {nom = value;}
+        }
+        public String Tramite
+        {
+            get {return tra;}
+            set {tra = value;}
+        }
+        public clsNodo Siguiente
+        {
+            get {return sig;}
+            set {sig = value;}
+        } 
+        */
+
+
+    }
+}
