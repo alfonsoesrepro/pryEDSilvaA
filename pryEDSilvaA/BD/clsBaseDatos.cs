02b7b433e3fa897343bbc612caa6fa972750fdd0
alfonsoesrepro <alfonsoesrepro@gmail.com>
2026-05-28 12:27:42 -0300

    feat: clase BaseDatos.

---
+++ pryEDSilvaA/BD/clsBaseDatos.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace pryEDSilvaA.BD
+{
+    internal class clsBaseDatos
+    {
+        private OleDbConnection conexion = new OleDbConnection();
+        private OleDbCommand comando = new OleDbCommand();
+        private OleDbDataAdapter adaptador = new OleDbDataAdapter();
+
+        private string CadenaConexion1 = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=BD/BD.mdb";
+        private string CadenaConexion2 = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=BD/BD.mdb";
+
+        public void Listar(DataGridView dgv)
+        {
+            try
+            {
+                conexion.ConnectionString = CadenaConexion1;
+                conexion.Open();
+
+                comando.Connection = conexion;
+                comando.CommandType = CommandType.TableDirect;
+                comando.CommandText = "Libro";
+
+                DataSet DS = new DataSet();
+                adaptador = new OleDbDataAdapter(comando);
+                adaptador.Fill(DS, "Libro");
+
+                dgv.DataSource = null;
+                dgv.DataSource = DS.Tables["Libro"];
+
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void Listar(String tabla, DataGridView dgv)
+        {
+            try
+            {
+                conexion.ConnectionString = CadenaConexion1;
+                conexion.Open();
+
+                comando.Connection = conexion;
+                comando.CommandType = CommandType.TableDirect;
+                comando.CommandText = tabla;
+
+                DataSet DS = new DataSet();
+                adaptador = new OleDbDataAdapter(comando);
+                adaptador.Fill(DS, tabla);
+
+                dgv.DataSource = null;
+                dgv.DataSource = DS.Tables[tabla];
+
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                conexion.Close();
+            }
+        }
+    }
+}
